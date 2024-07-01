4d146a8ba02861147837a3eadaa5f9ffc38051fb
BDNIKKO <moppertn@gmail.com>
2024-07-01 10:25:58 -0400

    added comments for review

--- traversing.js
+++ traversing.js
@@ -1,16 +1,25 @@
-
+// Select the first <header> element in the document
 const header = document.querySelector('header') 
 
+// Select all <section> elements in the document
 const sec = document.querySelectorAll('section') 
 
+// This line selects the <section> element with the class current and assigns it to the variable currentSec.
 const currentSec = document.querySelector('section.current') 
 
+// Select the next sibling element of the 'current' <section>
 const afterCurrent = currentSec.nextElementSibling 
 
+// Select the first child element of the previous sibling of the 'current' <section>
+// In this case, it's selecting the first <h2> element within the previous sibling <section>
 const hTwo = currentSec.previousElementSibling.children[0] 
 
+// Select the parent element of the 'current' <section>
+// This is likely the main container or a <div> that holds the sections
 const fullDiv = currentSec.parentElement 
 
+// Create an array from the NodeList of all <h2> elements, then map each <h2> to its parent element
+// The result is an array of all <section> elements that contain <h2> elements
 const h2Sections = Array.from(document.querySelectorAll('h2')).map(function(h2){return h2.parentElement}) 
 
 
